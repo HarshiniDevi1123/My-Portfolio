ab7ad5edd7c0652869b274c2742360651aa3d421
Kada Harshini Devi <khdevi1144@gmail.com>
2025-01-30 18:22:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings:["Developer","Designer","FrontEnd Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var typed = new Typed(".text", options);
